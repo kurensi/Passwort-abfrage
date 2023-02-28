8038a1331b6866ea9964824b138f77a165ed2d50
dejan <dejan@DESKTOP-HA22HU0>
2023-02-28 18:18:02 +0100

    Projektdateien hinzufügen.

---
+++ Passwort/Program.cs
@@ -0,0 +1,76 @@
+﻿
+//Passwort setze
+
+int k = 1, p = 2;
+do { 
+try
+{
+   
+    Console.WriteLine($"Geben sie ein Benutzername mit min 5 stellen ein: ");
+    Console.Write($"Setzen sie Ihr Benutzername: ");
+    string bn = Console.ReadLine();
+    Console.WriteLine($"Geben sie ein Passwort mit min 12 stellen ein: ");
+    Console.Write($"Setzen sie Ihr Passwort: ");
+    string pw = Console.ReadLine();
+    if (bn.Length >= 5 || bn.Length== 5 && pw.Length >= 12 || pw.Length == 12)
+    {
+        while (true)
+        {
+
+
+
+
+
+
+
+
+            //Eingabe bereich
+
+            Console.Write($"Geben sie Ihr Benutzername ein: ");
+            string eingb = Console.ReadLine();
+            Console.Write($"\nGeben sie Ihr Passwort ein: ");
+            string eingpw = Console.ReadLine();
+            Console.Clear();
+            if (eingb == bn && eingpw == pw)
+            {
+                Console.Write($"Erfolgreich eingeloggt");
+                break;
+
+            }
+            else
+            {
+                Console.WriteLine($"\n{k}te Falsche eingabe versuchen sie es erneut. {p} versuche übrig\n ");
+
+                k++;
+                p--;
+
+                if (k == 4)
+                {
+                    Console.WriteLine($"Zu viele falsche eingaben melden sie sich beim Support");
+                    break;
+                }
+            }
+
+
+
+
+
+        }
+
+    }
+
+    else
+    {
+        Console.WriteLine($"Falsche eingabe");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+    }
+while (true) ;
+
+
+
+
---
+++ passwort enebrocht.cs
@@ -0,0 +1,79 @@
+﻿
+//Passwort setze
+
+using System;
+
+int k = 1, p = 2;
+do { 
+try
+{
+   
+    Console.WriteLine($"Geben sie ein Benutzername mit min 5 stellen ein: ");
+    Console.Write($"Setzen sie Ihr Benutzername: ");
+    string bn = Console.ReadLine();
+    Console.WriteLine($"Geben sie ein Passwort mit min 12 stellen ein: ");
+    Console.Write($"Setzen sie Ihr Passwort: ");
+    string pw = Console.ReadLine();
+        Console.Clear();
+        if (bn.Length >= 4  && pw.Length >=11)
+    {
+        while (true)
+        {
+
+
+
+
+
+
+
+
+            //Eingabe bereich
+
+            Console.Write($"Geben sie Ihr Benutzername ein: ");
+            string eingb = Console.ReadLine();
+            Console.Write($"\nGeben sie Ihr Passwort ein: ");
+            string eingpw = Console.ReadLine();
+            Console.Clear();
+            if (eingb == bn && eingpw == pw)
+            {
+                Console.Write($"Erfolgreich eingeloggt");
+                break;
+
+            }
+            else
+            {
+                Console.WriteLine($"\n{k}te Falsche eingabe versuchen sie es erneut. {p} versuche übrig\n ");
+
+                k++;
+                p--;
+
+                if (k == 4)
+                {
+                    Console.WriteLine($"Zu viele falsche eingaben melden sie sich beim Support");
+                    break;
+                }
+            }
+
+
+
+
+
+        }
+
+    }
+
+    else
+    {
+        Console.WriteLine($"Falsche eingabe");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+    }
+while (true) ;
+
+
+
+
